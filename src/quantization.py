1b1248d1d82f988143cdb75f5ac51dc3016eeb0a
zaiman <zaiman.a.purnama@gmail.com>
2025-10-05 13:29:34 +0700

    fixed VGG using (now working with comparable accuracy to SOTA) and add Quantization for the bias

--- src/quantization.py
+++ src/quantization.py
@@ -69,6 +69,20 @@ class FastSign(nn.Module):
         return (out_forward.detach() 
                 - out_backward.detach() + out_backward)
 
+
+class Quantized(nn.Module):
+    '''
+    Quantize weight into a bitwidth
+    '''
+    def __init__(self, bits=5):
+        super(Quantized, self).__init__() 
+        self.bits = bits
+
+    def forward(self, input,):
+        input = torch.clamp(input, -1.0, 1.0)
+        input = torch.round((input*(2**(self.bits-1)-0.5))-0.5)
+        return input
+
 class QuantSign(torch.autograd.Function):
     '''
     Quantize Sign activation to arbitrary bitwidth.
@@ -76,7 +90,7 @@ class QuantSign(torch.autograd.Function):
         output = QuantSign.apply(input, bits)
     '''
     @staticmethod
-    def forward(ctx, input, bits=2):
+    def forward(ctx, input, bits=5):
         ctx.save_for_backward(input)
         input = torch.clamp(input, -1.0, 1.0)
         delta = 2.0/(2.0**bits-1.0)
@@ -97,35 +111,6 @@ class QuantSign(torch.autograd.Function):
         return grad_input, None
 
 
-##########
-##  Mask
-##########
-
-class SparseGreaterThan(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, input, threshold):
-        ctx.save_for_backward(input, torch.tensor(threshold))
-        return torch.Tensor.float(torch.gt(input, threshold))
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        input, threshold, = ctx.saved_tensors
-        grad_input = grad_output.clone()
-        ''' Identity gradients only when input >= threshold '''
-        grad_input *= (input>=threshold).float()
-        return grad_input, None
-
-class GreaterThan(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, input, threshold):
-        return torch.Tensor.float(torch.gt(input, threshold))
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        ''' Identity gradients '''
-        grad_input = grad_output.clone()
-        return grad_input, None
-
 
 ##########
 ##  Layer
@@ -143,52 +128,29 @@ class BinaryConv2d(nn.Conv2d):
                                               padding, dilation, groups,
                                               bias, padding_mode)
         self.binarize = FastSign()
+        self.quantize = Quantized(bits=5)
 
     def forward(self, input):
         return F.conv2d(input, self.binarize(self.weight),
-                        self.bias, self.stride, self.padding,
+                        self.quantize(self.bias), self.stride, self.padding,
                         self.dilation, self.groups)
+    
 
-class PGBinaryConv2d(nn.Conv2d):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=False,
-                 padding_mode='zeros', sparse_bp=True, init=-1.0):
-        super(PGBinaryConv2d, self).__init__(in_channels, out_channels,
-                                       kernel_size, stride,
-                                       padding, dilation, groups,
-                                       bias, padding_mode)
+
+class BinaryLinear(nn.Linear):
+    '''
+    A linear layer with its weight tensor binarized to {-1, +1}.
+    '''
+    def __init__(self, in_features, out_features, bias=True):
+        super(BinaryLinear, self).__init__(in_features, out_features, bias)
         self.binarize = FastSign()
-        self.gt = SparseGreaterThan.apply if sparse_bp else GreaterThan.apply
-
-        '''
-        zero initialization
-        nan loss while using torch.Tensor to initialize the thresholds
-        '''
-        self.threshold = nn.Parameter(torch.ones(1, out_channels, 1, 1)*init)
-
-        ''' number of output features '''
-        self.num_out = torch.zeros(1)
-        ''' number of output features computed at high precision '''
-        self.num_high = torch.zeros(1)
+        self.quantize = Quantized(bits=5)
 
     def forward(self, input):
-        ''' MSB convolution '''
-        out_msb = F.conv2d(self.binarize(input),
-                           self.binarize(self.weight),
-                           self.bias, self.stride, self.padding,
-                           self.dilation, self.groups) * 2.0 / 3.0
-        ''' Calculate the mask '''
-        mask = self.gt(torch.sigmoid(5.0*(out_msb-self.threshold)), 0.5)
-        ''' update report '''
-        self.num_out.fill_( mask.numel() )
-        self.num_high.fill_( (mask>0).sum().item() )
-        ''' full convolution '''
-        out_full = F.conv2d(input,
-                           self.binarize(self.weight),
-                           self.bias, self.stride, self.padding,
-                           self.dilation, self.groups)
-        ''' combine outputs '''
-        return (1-mask) * out_msb + mask * out_full
+        return F.linear(input, self.binarize(self.weight),
+                        self.quantize(self.bias))
+
+
 
 
 ##########
